d31e442e20b28534c0109da734152b3370df5dde
AlfianJe <zenvrog@gmail.com>
2023-01-03 00:01:10 +0700

    revisi3

--- index.php
+++ index.php
@@ -67,28 +67,26 @@
 
                 </div>
 
-                <div class="gender-inputs">
-                    <div class="gender-title">
-                        <h6>Gender</h6>
+                <div class="gender-details">
+                        <input type="radio" name="jenis_kelamin" value="Laki-Laki" id="dot-1">
+                        <input type="radio" name="jenis_kelamin" value="Perempuan" id="dot-2">
+                        <input type="radio" name="jenis_kelamin" value="null" id="dot-3">
+                        <span class="gender-title">Gender</span>
+                        <div class="category">
+                            <label for="dot-1">
+                                <span class="dot one"></span>
+                                <span class="gender">Male</span>
+                            </label>
+                            <label for="dot-2">
+                                <span class="dot two"></span>
+                                <span class="gender">Female</span>
+                            </label>
+                            <label for="dot-3">
+                                <span class="dot three"></span>
+                                <span class="gender">Prefer not to say</span>
+                            </label>
+                        </div>
                     </div>
-
-                    <div class="gender-group">
-                        <div class="gender-input">
-                            <input id="female" type="radio" name="gender">
-                            <label for="female">Female</label>
-                        </div>
-
-                        <div class="gender-input">
-                            <input id="male" type="radio" name="gender">
-                            <label for="male">Male</label>
-                        </div>
-
-                        <div class="gender-input">
-                            <input id="others" type="radio" name="gender">
-                            <label for="others">Prefer not to say</label>
-                        </div>
-                    </div>
-                </div>
                 
                 <div class="button">
                     <input type="submit" name="submit" value="submit">
