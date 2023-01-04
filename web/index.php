6ab83a4f7e8b29c024b1d2b00bcdc5056cd43dcc
Dea Fitria <deyafitriaa@gmail.com>
2023-01-04 23:19:26 +0700

    Update readme.md, initialized database/users/img

--- web/index.php
+++ web/index.php
@@ -8,7 +8,7 @@
     <title>Login</title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="asset/css/login.css">
+    <link rel="stylesheet" href="../asset/css/login.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
     
 </head>
