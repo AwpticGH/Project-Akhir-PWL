bf6734218d930b926597d7dcba42cf019f2b85ce
AlfianJe <zenvrog@gmail.com>
2023-01-05 22:10:25 +0700

    update komponen & foldering

---
+++ web/layout/sidebar.php
@@ -0,0 +1,11 @@
+<div class="border-end " id="sidebar-wrapper">
+    <div class="sidebar-heading border-bottom"><i class='bx bxs-pencil'></i>&nbsp; E-Kinerja</div>
+    <div class="list-group list-group-flush">
+        <a class="list-group-item list-group-item-action list-group-item-light p-3 active" style="background-color: #000;" href="#!">Dashboard</a>
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" style="background-color: #00203F;" href="#!">Shortcuts</a>
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" style="background-color: #00203F;" href="#!">Overview</a>
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" style="background-color: #00203F;" href="#!">Events</a>
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" style="background-color: #00203F;" href="#!">Profile</a>
+        <a class="list-group-item list-group-item-action list-group-item-light p-3" style="background-color: #00203F;" href="#!">Status</a>
+    </div>
+</div>
