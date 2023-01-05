a1e475f0b9e781d5df62812ee0fae17615bd3d9b
AlfianJe <zenvrog@gmail.com>
2023-01-06 01:53:10 +0700

    update dashboard dan nambah file create dan show

--- web/employee/dashboard.php
+++ web/employee/dashboard.php
@@ -23,7 +23,7 @@
                             <h1 class="mt-4">Dashboard Pegawai</h1>
                             <div class="card1">
                                 <div class="form-image">
-                                <img src="../asset/img/img5.svg" alt="">
+                                <img src="../../asset/img/img5.svg" alt="">
                                 </div>
                             </div>
                             <div class="card4">
@@ -32,37 +32,70 @@
                                         <div class="card5">
                                             <div class="row">
                                                 <div class="col-6">
-                                                    halo
+                                                    <img src="../../asset/img/terima.svg" alt="logo" style="margin-left: 20px;">
                                                 </div>
                                                 <div class="col-6">
-                                                    halo
+                                                    0
+                                                    <br>
+                                                    Laporan Diterima
                                                 </div>
                                             </div>
                                         </div>
                                     </div>
                                     <div class="col-4">
                                         <div class="card6">
-                                            halo
+                                            <div class="row">
+                                                <div class="col-6">
+                                                    <img src="../../asset/img/tolak.svg" alt="logo" style="margin-left: 20px;">
+                                                </div>
+                                                <div class="col-6">
+                                                    0
+                                                    <br>
+                                                    Laporan Ditolak
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                     <div class="col-4">
                                         <div class="card7">
-                                            halo
+                                            <div class="row">
+                                                <div class="col-6">
+                                                    <img src="../../asset/img/tunggu.svg" alt="logo" style="margin-left: 20px;">
+                                                </div>
+                                                <div class="col-6">
+                                                    0
+                                                    <br>
+                                                    Laporan In Progress
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
+                            </div>
+                            <div class="card4">
+                                Table
                             </div>
                         </div>
                         <!-- <div class="col-1"> -->
                         <div class="card2">
                             <div class="card3">
-                                halo
+                                <center>
+                                    Zaki ketoprak
+                                    <br>
+                                    Username
+                                </center>
                             </div>
                             <div class="card3">
-                                halo
+                                <center>
+                                    Divisi
+                                    <br>
+                                    Jabatan
+                                </center>
                             </div>
                             <div class="card3">
-                                halo
+                                <center>
+                                    100%
+                                </center>
                             </div>
                         </div>
                     </div>
