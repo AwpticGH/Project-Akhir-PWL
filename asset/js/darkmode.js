5560d3fa27bee80ea892e7edb1d1107fb167ac75
fauzanmhr <fauzan432003@gmail.com>
2023-01-08 02:02:38 +0700

    merge process

--- asset/js/darkmode.js
+++
@@ -1,9 +0,0 @@
-var icon = document.getElementById("icon");
-icon.onclick = function () {
-    document.body.classList.toggle("modegelap");
-    if (document.body.classList.contains("modegelap")) {
-        icon.src = "../../asset/img/sun.png";
-    } else {
-        icon.src = "../../asset/img/moon.png";
-    }
-}
