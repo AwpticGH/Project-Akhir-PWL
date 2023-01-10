1b22d22d35c011d05e1e56d6873b515cd2f28c83
AlfianJe <zenvrog@gmail.com>
2023-01-10 16:01:31 +0700

    fix dropwdown navbar

--- web/layout/navbar.php
+++ web/layout/navbar.php
@@ -5,21 +5,18 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"></button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav ms-auto mt-2 mt-lg-0">
-            <img src="../../asset/img/moon.png" alt="iconcoy" id="icon">
-            <li class="nav-link"> <box-icon name='bell' color="white" size="30px"></box-icon> </li>
-                <li class=" btn btn-rounded nav-item dropdown" style="background-color: #ADEFD1">
-                    <a class="nav-link dropdown-toggle" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                        <div style="float: left;">
-                        <div class="profile">
-                        <box-icon size="20px" name='user-circle'></box-icon>
-                            <span class="name">People</span>
-                        </div>
-                    </div>
-                    <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                        <a class="dropdown-item" href="#!">Edit Profile</a>
+                <img src="../../asset/img/moon.png" alt="iconcoy" id="icon">
+                <li class="nav-link"> <box-icon name='bell' color="white" size="30px"></box-icon></li>
+                <li class="dropdown nav-link">
+                    <div class="dropdown">
+                    <a class="btn dropdown-toggle" style="background-color: #ADEFD1; color: black; border-radius: 12px;" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    <span> <box-icon size="15px" name='user-circle'></box-icon> Profile </span>
+                    </a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="#">Edit Profile</a></li>
                         <hr>
-                        <!-- <div class="dropdown-divider"></div> -->
-                        <a class="dropdown-item" style="color: red; font-size: 18px;" href="#"><i class='bx bx-log-out-circle'></i>Logout</a>
+                        <li><a class="dropdown-item" style="color: red; font-size: 18px;" href="#"><i class='bx bx-log-out-circle'></i>Logout</a></li>
+                    </ul>
                     </div>
                 </li>
             </ul>
