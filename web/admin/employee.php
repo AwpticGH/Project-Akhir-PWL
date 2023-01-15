05c400dd93725ab9d60e70cfeb71304527a2a3a6
Rafi Fajar Sulaiman <rafihariyadi01@gmail.com>
2023-01-15 18:06:13 +0700

    backend update rafi
    1. nambahin item sidebar buat admin
    
    2. cleaned files

--- web/admin/employee.php
+++ web/admin/employee.php
@@ -1,3 +1,7 @@
+<?php
+    $page_for = "admin";
+    include("../layout/starter.php");
+?>
 <!DOCTYPE html>
 <html lang="en">
 
--- web/layout/sidebar.php
+++ web/layout/sidebar.php
@@ -1,13 +1,14 @@
 <div class="border-end " id="sidebar-wrapper">
     <div class="sidebar-heading border-bottom"><i class='bx bxs-pencil'></i>&nbsp; E-Kinerja</div>
     <div class="list-group list-group-flush">
-        <?php if ($user['position_id'] == "1") { ?>
+        <?php if ($user['position_id'] == "1") { // Head ?>
             <a class="list-group-item list-group-item-action list-group-item-light p-3 active" href="../admin/dashboard.php">Dashboard</a>
             <a class="list-group-item list-group-item-action list-group-item-light p-3"  href="../presence/confirmation.php">Confirm Presence</a>
             <a class="list-group-item list-group-item-action list-group-item-light p-3"  href="../report/show.php">Check Reports</a>
+            <a class="list-group-item list-group-item-action list-group-item-light p-3"  href="../admin/employee.php">See Employees</a>
             <a class="list-group-item list-group-item-action list-group-item-light p-3"  href="../auth/show.php?id=<?= $user['id'] ?>">Profile</a>
         <?php } ?>
-        <?php if ($user['position_id'] == "2") { ?>
+        <?php if ($user['position_id'] == "2") { // Employee ?>
             <a class="list-group-item list-group-item-action list-group-item-light p-3 active" href="../employee/dashboard.php">Dashboard</a>
             <a class="list-group-item list-group-item-action list-group-item-light p-3"  href="../presence/create.php">Presence</a>
             <a class="list-group-item list-group-item-action list-group-item-light p-3"  href="../report/create.php">Make Report</a>
