5fdd7fc95f0351a5b63760ff8da798f4e3af1e95
dvaled <davaalif0@gmail.com>
2023-01-17 00:54:55 +0700

    update dava kurang create presence

--- controller/PresencesController.php
+++ controller/PresencesController.php
@@ -2,29 +2,72 @@
     namespace controller;
 
     require_once("../../config/DBConfig.php");
-    use config\DBConfig;
+    use config\DBConfig as DBConfig;
     require_once("../../query/PresencesQuery.php");
-    use query\PresencesQuery;
+    use query\PresencesQuery as PresencesQuery;
 
     class PresencesController {
+
+        public $error = "";
+        private $conn = null;        
         
-        private $conn = null;
-
         // function create
+        public function create($date_of_presence, $t ) {
+            $this -> conn = DBconfig::connect();
+            $sql = PresencesQuery::$createabsen;
+            $stmt = mysqli_prepare($this -> conn, $sql);
+            $stmt -> bind_param("ss", $date_of_presence, $t);
 
+            return $stmt -> execute();
+        }
         // function update
-
-        // function readAllByUserId
-
-        // count score
-        public function countScoreByUserId($user_id) {
+        public function updateAccept($presences_acc) {
             $this -> conn = DBConfig::connect();
-            $sql = PresencesQuery::$countScoreByUserId;
-            $stmt = mysqli_prepare($this->conn, $sql);
-            $stmt -> bind_param("s", $user_id);
-            $stmt -> execute();
+            $sql = PresencesQuery::$accabsen;
+            $stmt = mysqli_prepare($this-> conn, $sql);
+            $stmt -> bind_param("s", $presences_acc);
 
+            return $stmt -> execute();
+        }
+
+        public function updateReject($presences_rjc) {
+            $this -> conn = DBConfig::connect();
+            $sql = PresencesQuery::$rjcabsen;
+            $stmt = mysqli_prepare($this-> conn, $sql);
+            $stmt -> bind_param("s", $presences_rjc);
+
+            return $stmt -> execute();
+        }
+        // function readAllByUserId
+        public function readByDivisionId($division_id) {
+            $this -> conn = DBConfig::connect();
+            $sql = PresencesQuery::$readByDivisionId;
+            $stmt = mysqli_prepare($this->conn, $sql);
+            $stmt -> bind_param("s", $division_id);
+            $stmt -> execute(); 
+            
             return $stmt -> get_result();
         }
-    }
+        public function readByUserId($user_id) {
+            $this -> conn = DBConfig::connect();
+            $sql = PresencesQuery::$readByUserId;
+            $stmt = mysqli_prepare($this->conn, $sql);
+            $stmt -> bind_param("s", $user_id);
+            $stmt -> execute(); 
+            
+            return $stmt -> get_result();
+        }
+        
+        
+        // count score absen
+        public static $countScoreByUserId = "SELECT (
+            SELECT COUNT(*) 
+            FROM `presences` 
+            WHERE user_id = ?
+            AND presences.date_of_presence < STR_TO_DATE(CONCAT(YEAR(CURRENT_DATE), '-', MONTH(CURRENT_DATE), '-25'), '%Y-%m-%e')
+            AND presences.date_of_presence > STR_TO_DATE(CONCAT(IF(MONTH(CURRENT_DATE) = 1, YEAR(DATE_ADD(CURRENT_DATE, INTERVAL -1 YEAR)), YEAR(CURRENT_DATE)), '-', MONTH(DATE_ADD(CURRENT_DATE, INTERVAL -1 MONTH)), '-25'), '%Y-%m-%e')
+            ) / 30 * 100
+            AS total_score";
+}
+        
 ?>
--- web/layout/sidebar.php
+++ web/layout/sidebar.php
@@ -11,6 +11,7 @@
         <?php if ($user['position_id'] == "2") { // Employee ?>
             <a class="list-group-item list-group-item-action list-group-item-light p-3 active" href="../employee/dashboard.php">Dashboard</a>
             <a class="list-group-item list-group-item-action list-group-item-light p-3"  href="../presence/create.php">Presence</a>
+            <a class="list-group-item list-group-item-action list-group-item-light p-3"  href="../presence/statusabsen.php">Presence status</a>
             <a class="list-group-item list-group-item-action list-group-item-light p-3"  href="../report/create.php">Make Report</a>
             <a class="list-group-item list-group-item-action list-group-item-light p-3"  href="../auth/show.php?id=<?= $user['id'] ?>">Profile</a>
         <?php } ?>
