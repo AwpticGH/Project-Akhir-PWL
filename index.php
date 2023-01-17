47f00280c828448d9c4facfc03b933058bec109f
fauzanmhr <fauzan432003@gmail.com>
2023-01-18 01:50:26 +0700

    .

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link href='asset/css/landing.css' rel='stylesheet'>
+<link href='asset/css/bootstrap.css' rel='stylesheet' />
+<title>Landing</title>
+</head>
+<body>
+   
+    <Center>
+            <img src="asset/img/imgL.jpg" alt="Home page" class="cssimg">
+        <div class="row">
+        
+        <div class="d-grid gap-2 col-6 mx-auto">
+            <a href="web/auth/index.php" class="btn btn-light" role="button" style="background-color:#ADEFD1; border-radius:50px;padding:20px;border:black solid">Sign In</a>
+            </div>
+        </div>
+    </Center>
+</body>
+</html>
