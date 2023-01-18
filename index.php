0941fbb60465756c71a68e513c4f0ac700c4e5ff
fauzanmhr <fauzan432003@gmail.com>
2023-01-18 08:46:46 +0700

    update landing page

--- index.php
+++ index.php
@@ -1,20 +1,30 @@
 <!DOCTYPE html>
-<html lang="en">
+<meta charset="utf-8">
+<html>
 <head>
-<link href='asset/css/landing.css' rel='stylesheet'>
-<link href='asset/css/bootstrap.css' rel='stylesheet' />
 <title>Landing</title>
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+<style type="text/css">
+    .landing-logo {
+    margin-top: 250px;
+    line-height: 1;
+    color:#475f7b ;
+    font-size: 5.5rem;
+    text-align: center!important;
+    font-family: Raleway;
+    box-sizing: border-box;
+
+    }
+</style>
 </head>
 <body>
-   
-    <Center>
-            <img src="asset/img/imgL.jpg" alt="Home page" class="cssimg">
-        <div class="row">
-        
-        <div class="d-grid gap-2 col-6 mx-auto">
-            <a href="web/auth/index.php" class="btn btn-light" role="button" style="background-color:#ADEFD1; border-radius:50px;padding:20px;border:black solid">Sign In</a>
-            </div>
-        </div>
-    </Center>
+<center>
+<div class="landing-logo">
+<p>E-Kinerja</p>
+<p style="font-size: 20px;font-style: italic">silahkan login terlebih dahulu</p>
+</div>
+<br>
+<a type="button" href="web/auth/index.php" class="btn btn-dark" style="width: 5rem; background-color: #475f7b">login</a></center>
+</div>
 </body>
 </html>
